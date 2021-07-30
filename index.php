16c418e3d4028cb185cff22ff33941d53789f203
Elizabeth-09 <2002.k.elizabeth@gmail.com>
2021-07-30 09:38:50 -0600

    index

--- index.php
+++ index.php
@@ -4,7 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <!.. CSS>
+    <link rel="stylesheet" href="./public/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="./public/css/boostrap-theme.css">
+
+    <!.. JS ..>
+    <script src="./public/js/jquery-3.5.1.slim.min.js"></script> 
+    <script src="./public/js/jquery-1.9.1.min.js"></script>
+    <script src="./public/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
+
 </head>
 <body>
     
